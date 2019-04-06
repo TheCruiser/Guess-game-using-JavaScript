b2ce57f6ce3b14ffa30b76dccd430b175bd65123
Satish Ranjan Rout <36737598+TheCruiser@users.noreply.github.com>
2019-04-06 07:41:07 +0530

    Add files via upload

---
+++ guessgame.js
@@ -0,0 +1,13 @@
+var num=7;
+var guess=prompt("Guess a number between 0-9");
+if(guess>7)
+{
+	alert("It's too high");
+}
+else if(guess<7)
+{
+	alert("It's too low");
+}
+else{
+	alert("You guessed it right!!! Congratulations :)");
+}
